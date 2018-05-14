ff910cd8af0bcd97ce218282e6d2ac2c790c684c
Ben Furfie <ben.furfie@textlocal.com>
2018-05-14 13:17:55 +0100

    initial commit

---
+++ routes/web.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\DB;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function ()
+{
+    $tasks = DB::table('tasks')->latest()->get();
+    // return $tasks;
+    
+    return view('welcome', compact('tasks'));
+});
+
+Route::get('/tasks', function ()
+{
+    $tasks = DB::table('tasks')->get();
+    // return $tasks;
+    
+    return view('tasks.index', compact('tasks'));
+});
+
+Route::get('/tasks/{id}', function($id)
+{
+    $task = DB::table('tasks')->find($id);
+ 
+    return view('tasks.show', compact('task'));
+});
